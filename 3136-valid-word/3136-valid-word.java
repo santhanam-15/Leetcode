68a5b37eb6946ecec9b9b65a4d3cd5cc47e2f5ba
Santhana Pandi <santhanamkavya9@gmail.com>
2026-05-13 14:59:46 +0530

    Time: 1 ms (99.82%), Space: 43.2 MB (82.91%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isValid(String word) {
-        int vow = 0, cons = 0;
-        if(word.length() < 3) return false;
-        for(char c: word.toCharArray()){
-            if(Character.isLetter(c)){
-                if("AEIOUaeiou".indexOf(c) != -1) vow++;
-                else cons++;
+        if (word.length() < 3) return false;
+        boolean vow = false, cons = false;      
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if(vow && cons) continue ; 
+                if ("AEIOUaeiou".indexOf(c) != -1) {
+                    vow = true;
+                } else {
+                    cons = true;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
             }
-            else if(!Character.isDigit(c)) return false;
         } 
-        return vow >= 1 && cons >= 1;       
+        return vow && cons;       
     }
 }
