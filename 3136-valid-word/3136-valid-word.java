bee73d91057bdce8051772eab24bd6c94b8ef896
Santhana Pandi <santhanamkavya9@gmail.com>
2026-05-13 15:05:42 +0530

    Time: 1 ms (99.82%), Space: 43.3 MB (69.6%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -1,15 +1,24 @@
 class Solution {
-    public boolean isValid(String word) {
-        if (word.length() < 3) return false;
-        boolean vow = false, cons = false;      
-        for (char c : word.toCharArray()) {
-            if (Character.isLetter(c)) {
-                if(vow && cons) continue ; 
-                if ("AEIOUaeiou".indexOf(c) != -1) vow = true;
-                else cons = true;
+    public boolean isValid(String word) {       
+        if(word.length() < 3) return false;
+        boolean vow = false, cons = false;
+        for(char c : word.toCharArray()) {
+            switch(c) {
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    vow = true;
+                    break;
+                case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 
+                     'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+                     'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 
+                     'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z':
+                    cons = true;
+                    break;
+                case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+                    break;
+                default:
+                    return false;
             }
-            else if (!Character.isDigit(c)) return false;
-        } 
-        return vow && cons;       
+        }
+        return vow && cons ;
     }
 }
