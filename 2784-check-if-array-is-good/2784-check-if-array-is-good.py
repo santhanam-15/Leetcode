c476da772e4991249606225f391487d60fbfb362
Santhana Pandi <santhanamkavya9@gmail.com>
2026-05-14 12:35:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.79%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,7 +1,5 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        n = max(nums)
-        freq = Counter(nums)
-        for i in range(1, n):
-            if freq[i] != 1:  return False
-        return freq[n] == 2
+        n = len(nums) - 1
+        target = list(range(1, n + 1)) + [n]      
+        return sorted(nums) == target
