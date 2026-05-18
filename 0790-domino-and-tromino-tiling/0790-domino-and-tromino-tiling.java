718b740247d53c3b0d956b1d485f50b0779a5dd9
Santhana Pandi <santhanamkavya9@gmail.com>
2026-05-18 18:02:38 +0530

    Time: 1 ms (60.04%), Space: 42.4 MB (29.13%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -3,20 +3,16 @@ class Solution {
     int MOD = 1_000_000_007;
     public int numTilings(int n) {
         dp=new int[n+1];
-        return rep(n);
-    }
-    int rep(int n){
-        if (n==0){
-            return 1;
-        }
         if(n<=2){
-            return n;
+            return n==0 ? 0 : n;
         }
-        if(dp[n]!=0){
-            return dp[n];
+
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (int) ((2L * dp[i - 1] + dp[i - 3]) % MOD);
         }
-        long res = (2L * rep(n - 1) + rep(n - 3)) % MOD; 
-        dp[n]=(int)res;
         return dp[n];
     }
 }
