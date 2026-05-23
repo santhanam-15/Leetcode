1186b713d935cfebbbc8853183e54065ce059e47
Santhana Pandi <santhanamkavya9@gmail.com>
2026-05-23 18:51:42 +0530

    Time: 21 ms (82%), Space: 50.9 MB (89.99%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,17 +1,17 @@
 class RecentCounter {
-    List<Integer> counter=new LinkedList<>();
+    Deque<Integer> counter = new ArrayDeque<>();
     public RecentCounter() {
     }
     
     public int ping(int t) {
         if(counter.isEmpty()){
-            counter.add(t);
+            counter.addLast(t);
             return 1;
         }
         counter.add(t);
         int l= t-3000;
-        while(counter.get(0)<l){
-            counter.remove(0);
+        while(counter.getFirst()<l){
+            counter.removeFirst();
         }
         return counter.size();
     }
